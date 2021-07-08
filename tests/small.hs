05f68a0d068d1c8d8a6658c1c92ded252903e771
Mitsutoshi Aoe <me@maoe.name>
2021-07-08 12:43:45 +0900

    Rewrite the tests with tasty + HUnit-approx
    Also one more test case is added.

--- tests/small.hs
+++ tests/small.hs
@@ -1,61 +1,127 @@
+{-# LANGUAGE ImplicitParams #-}
 import Control.Monad
 import qualified Data.Vector.Storable as VS
 import Numeric.LinearAlgebra
-import Numeric.Minimization.QuadProgPP
+    ( (><), ident, Additive(add), Linear(scale) )
+import Numeric.Minimization.QuadProgPP ( solveQuadProg )
+import Test.Tasty ( defaultMain, testGroup, TestTree )
+import Test.Tasty.HUnit ( testCase, assertEqual, assertFailure )
+import Test.HUnit.Approx ( (@~?) )
+import Debug.Trace
+import Numeric.LinearAlgebra.HMatrix (tr)
 
-main = do
-  test 1e-12 answer0 expected0
-  test 1e-5 answer1 expected1
-  where
-    test tol answer expected =
-      when (either (const True) (bad . fst) answer) $ print (answer, expected)
-      where
-        bad x = VS.length x /= VS.length expected
-          || VS.any ((>tol) . abs) (VS.zipWith (-) x expected)
+main :: IO ()
+main = defaultMain tests
 
-    -- Problem 0
-    -- minimize: 2(x-1)^2 + (y-4)^2 
-    -- s.t.: x >= 0, y >= 0, x + 2y <= 2
-    answer0 = solveQuadProg
-      ( (2><2)
-        [ 4, 0
-        , 0, 2
-        ]
-        , VS.fromList [-4, -8])
-      Nothing
-      (Just
-        ((3><2)
-          [ 1, 0
-          , 0, 1
-          ,-1,-2
-          ]
-        , VS.fromList [0, 0, 2]))
+tests :: TestTree
+tests = testGroup "small"
+    -- [ problem0
+    [ problem1
+    -- , problemQuadprogppDemo
+    ]
 
-    expected0 = VS.fromList [2/9, 8/9]
+-- Problem 0
+-- minimize: 2(x-1)^2 + (y-4)^2
+-- s.t.: x >= 0, y >= 0, x + 2y <= 2
+problem0 :: TestTree
+problem0 = testCase "Problem 0" $ do
+    let answer = solveQuadProg
+            ( (2><2)
+                [ 4, 0
+                , 0, 2
+                ]
+            , VS.fromList [-4, -8]
+            )
+            Nothing
+            ( Just
+                ( (3><2)
+                    [ 1, 0
+                    , 0, 1
+                    ,-1,-2
+                    ]
+                , VS.fromList [0, 0, 2]
+                )
+            )
+    (actual, best) <- case answer of
+        Left err -> assertFailure $ show err
+        Right x -> return x
+    let ?epsilon = 1e-12
+    let expected = VS.fromList [2/9, 8/9] :: VS.Vector Double
+    assertEqual "The answer should have the expected length"
+        (VS.length expected)
+        (VS.length actual)
+    VS.zipWithM_ (@~?) (traceShowId expected) (traceShowId actual)
 
-    -- Problem 1
-    -- Same as Problem 0, but substitute:
-    --  s = x
-    --  t = x + y
-    --  u = x - y
-    -- and add a small positive constant to the diagonal elements to keep
-    -- the problem positive definite.
-    answer1 = solveQuadProg
-      ( ((3><3)
-        [   1, 2/3, 1/3
-        , 2/3, 2/3,   0
-        , 1/3,   0, 1/3
-        ] `add` (1e-12 `scale` ident 3))
-        , VS.fromList [-2, -4, 2]
-        )
-      (Just
-        ((1><3) [-3, 2, 1]
-        , VS.fromList [0]))
-      (Just
-        ((3><3)
-          [ 1,    0,    0
-          , 0,  1/3, -1/3
-          , 0, -4/3,  1/3
-          ]
-        , VS.fromList [0, 0, 2]))
-    expected1 = VS.fromList [2/9, 10/9, -14/9]
+
+-- Problem 1
+-- Same as Problem 0, but substitute:
+--  s = x
+--  t = x + y
+--  u = x - y
+-- and add a small positive constant to the diagonal elements to keep
+-- the problem positive definite.
+problem1 :: TestTree
+problem1 = testCase "Problem 1" $ do
+    let answer = solveQuadProg
+            ((3><3)
+                [   1, 2/3, 1/3
+                , 2/3, 2/3,   0
+                , 1/3,   0, 1/3
+                ] `add` (1e-12 `scale` ident 3)
+            , VS.fromList [-2, -4, 2]
+            )
+            (Just
+                ( (1><3) [-3, 2, 1]
+                , VS.fromList [0]
+                )
+            )
+            (Just
+                ((3><3)
+                    [ 1,    0,    0
+                    , 0,  1/3, -1/3
+                    , 0, -4/3,  1/3
+                    ]
+                , VS.fromList [0, 0, 2]
+                )
+            )
+    (actual, best) <- case answer of
+        Left err -> assertFailure $ show err
+        Right x -> return x
+    let expected = VS.fromList [2/9, 10/9, -14/9] :: VS.Vector Double
+    assertEqual "The answer should have the expected length"
+        (traceShowId $ VS.length expected)
+        (traceShowId $ VS.length actual)
+    let ?epsilon = 1e-5
+    VS.zipWithM_ (@~?) (traceShowId expected) (traceShowId actual)
+
+problemQuadprogppDemo :: TestTree
+problemQuadprogppDemo = testCase "Problem quadpropp demo" $ do
+    let answer = solveQuadProg
+            ((2><2)
+                [  4, -2
+                , -2,  4 ]
+            , VS.fromList [6, 0]
+            )
+            (Just
+                ( (1><2) [1, 1]
+                , VS.fromList [-3]
+                )
+            )
+            (Just
+                ((3><2)
+                    [ 1, 0
+                    , 0, 1
+                    , 1, 1 ]
+                , VS.fromList [0, -2, 0]
+                )
+            )
+    (actual, best) <- case answer of
+        Left err -> assertFailure $ show err
+        Right x -> return x
+    let ?epsilon = 1e-5
+    12 @~? best
+    let expected = VS.fromList [1, 2] :: VS.Vector Double
+    assertEqual "The answer should have the expected length"
+        (traceShowId $ VS.length expected)
+        (traceShowId $ VS.length actual)
+    VS.zipWithM_ (@~?) (traceShowId expected) (traceShowId actual)
